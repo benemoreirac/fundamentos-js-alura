158ddb0d651d7b5bedd382f7fe1a2e7ae1eed427
Ebenezer Cavalcante <ebenezer.moreira90@gmail.com>
2023-06-19 11:19:58 -0300

    template literal, manipulação de erros, operador ternario

---
+++ template-strings.js
@@ -0,0 +1,8 @@
+const nome = "Ebenézer";
+const idade = 2023 - 1990;
+const cidadeDeNascimento = "Patrocínio / MG";
+
+
+const apresentacao = `Meu nome é ${nome} tenho ${idade} anos, natural de ${cidadeDeNascimento}.`
+
+console.log(apresentacao);
